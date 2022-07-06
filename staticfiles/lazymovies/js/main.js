215a68b25059a8ebd7d477e73b318b4f3cad4721
NahuelA <nahuelarrascaeta22@gmail.com>
2022-07-06 12:56:09 -0300

    ADD few changes and functionality categories

--- staticfiles/lazymovies/js/main.js
+++ staticfiles/lazymovies/js/main.js
@@ -1,66 +1,44 @@
-import { API_KEY } from "./config.js";
+import { API_KEY } from './config.js'
 
 // IMG size
-export const img_size_3 = "https://image.tmdb.org/t/p/w500/";
+export const img_size_3 = 'https://image.tmdb.org/t/p/w500/'
 
 // Query selector
 export const $ = (id) => {
-  return document.querySelector(id);
-};
+    return document.querySelector(id)
+}
 
 // Axios use
 export const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-  },
-  params: {
-    api_key: `${API_KEY}`,
-  },
-});
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        api_key: `${API_KEY}`,
+    },
+})
 
 // Create elements
 export const create_node = (elemn) => {
-  return document.createElement(elemn);
-};
+    return document.createElement(elemn)
+}
 
 /* Get elements from navigation */
-export const go_back_lazygram = $("#gb-lazygram");
-export const home_menu = $("#home-menu");
-/* Categories */
-
-// export const ctgy_action = $('#28').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_adventure = $('#12').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_animation = $('#16').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_comedy = $('#35').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_crime = $('#80').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_documentary = $('#99').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_drama = $('#18').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_family = $('#10751').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_fantasy = $('#14').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_history = $('#36').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_horror = $('#27').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_music = $('#10402').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_mystery = $('#9648').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_romance = $('#10749').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_science_fiction = $('#878').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_tv_movie = $('#10770').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_thriller = $('#53').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_war = $('#10752').addEventListener('click', ()=>{category_select_view(37)})
-// export const ctgy_western = $('#37').addEventListener('click', ()=>{category_select_view(37)})
-
+export const go_back_lazygram = $('#gb-lazygram')
+export const home_menu = $('#home-menu')
 /* Top rated */
-export const trends = $("#trends");
+export const trends = $('#trends')
 /* Search button */
-export const search_btn = $("#search-mv");
-export const inp_search_mv = $("#inp-search-mv");
+export const search_btn = $('#search-mv')
+export const inp_search_mv = $('#inp-search-mv')
 /* Title window */
-export const title_window = $("#title-window");
+export const title_window = $('#title-window')
 
 /* Containers for do not overload */
 
-export const main = $("#main");
+export const main = $('#main')
 export const aside = $('#aside-bar')
 
 // Trends
-export const container_trends_main = $("#container-trends");
+export const container_trends_main = $('#container-trends')
--- staticfiles/lazymovies/js/navigation.js
+++ staticfiles/lazymovies/js/navigation.js
@@ -1,5 +1,5 @@
 // Navigation logic, to change the resources consumed
-import { 
+import {
     // Get elements from navigation menu
     home_menu,
     main,
@@ -7,60 +7,58 @@ import {
     search_btn,
     inp_search_mv,
     go_back_lazygram,
-
-} from "./main.js"
+} from './main.js'
 
 // Functions view
-import { 
-         // Views to home
-         get_trends_home,
-         // Views to trends
-         get_home,
-         get_views,
-         categories_view,
-        } from "./views.js"
+import {
+    // Views to home
+    get_trends_home,
+    // Views to trends
+    get_home,
+    get_views,
+    categories_view,
+    category_select_view,
+} from './views.js'
 
-const hashChange = () =>{
-
-    if (location.hash.startsWith('#trends')){
-        get_views('/trending/movie/day','Trends')
-    }
-    else if (location.hash.startsWith('#top_rated')){
-        get_views('/movie/top_rated','Top rated')
-    }
-    else if (location.hash.startsWith('#category=')){
-    }
-    else if (location.hash.startsWith('#search=')) get_search_res_view(inp_search_mv.value)
+const hashChange = () => {
+    if (location.hash.startsWith('#trends')) {
+        get_views('/trending/movie/day', 'Trends')
+    } else if (location.hash.startsWith('#top_rated')) {
+        get_views('/movie/top_rated', 'Top rated')
+    } else if (location.hash.startsWith('#category=')) {
+        category_select_view(parseInt(location.hash.slice(10)))
+        categories_view()
+    } else if (location.hash.startsWith('#search='))
+        get_search_res_view(inp_search_mv.value)
     else get_home_view()
 }
 
 // Events Hash change in window-DOM
 window.addEventListener('hashchange', hashChange, false)
-window.addEventListener('DOMContentLoaded',hashChange, false)
+window.addEventListener('DOMContentLoaded', hashChange, false)
 
 // Events click change hash
 
 /* Home hash*/
-home_menu.addEventListener('click',()=>{
+home_menu.addEventListener('click', () => {
     location.hash = '#home'
 })
 
 /* Sub categories hash*/
 
 /* Top rated hash*/
-trends.addEventListener('click',()=>{
+trends.addEventListener('click', () => {
     location.hash = '#trends'
 })
 
 /* Search has*/
-search_btn.addEventListener('click',()=>{
+search_btn.addEventListener('click', () => {
     location.hash = `#search=${inp_search_mv.value}`
 })
 
 /* GO BACK LAZYGRAM APP*/
-go_back_lazygram.addEventListener('click',(e)=>{
-
-    const scheme = location.protocol+'//'
+go_back_lazygram.addEventListener('click', (e) => {
+    const scheme = location.protocol + '//'
     const url = `${scheme}${location.hostname}:${location.port}/home/`
     location.href = url
     location.replace(url)
@@ -70,21 +68,17 @@ go_back_lazygram.addEventListener('click',(e)=>{
 
 /* HOME VIEW*/
 const get_home_view = async () => {
-
     main.innerHTML = ''
     await get_trends_home()
+    // Load categories
     await categories_view()
-    const ctgy = document.querySelectorAll('.ctgy').forEach((category)=>{
-
-        console.log(category)
-    })
     await get_home('/movie/top_rated', 'Top rated', true)
     await get_home('/movie/popular', 'Most popular')
     await get_home('/movie/upcoming', 'Upcoming')
 }
 
 /* SEARCH VIEW*/
-const get_search_res_view = (mv)=>{
+const get_search_res_view = (mv) => {
     console.log(mv)
 }
 
